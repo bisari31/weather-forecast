c146117c826591de3cd1ac09247d34307e06c8ff
bisari <bisari31@gmail.com>
2022-06-17 18:25:55 +0900

    Design: 불필요한 css 정리

--- src/routes/chartList/ChartItem.tsx
+++ src/routes/chartList/ChartItem.tsx
@@ -34,32 +34,30 @@ const ChartItem = ({ mean, active, data }: IProps) => {
   if (!active) return null
 
   return (
-    <div className={cx(styles.chart, { [styles.active]: active })}>
-      <ResponsiveContainer>
-        <AreaChart margin={{ top: 20, right: 5, left: 5, bottom: 0 }} data={newData}>
-          <defs>
-            <linearGradient id='color' x1='0' y1='0' x2='0' y2='1'>
-              <stop offset='11%' stopColor='#fed057' stopOpacity={0.3} />
-              <stop
-                offset='85%'
-                stopColor={`${document.documentElement.getAttribute('theme') === 'day' ? '#3c6ad4' : '#101039'}`}
-                stopOpacity={0.1}
-              />
-            </linearGradient>
-          </defs>
-          <XAxis
-            padding={{ left: 10, right: 10 }}
-            stroke='#ffffff99'
-            tickLine={false}
-            tick={{ fill: '#ffffff99' }}
-            dataKey='x'
-          />
-          <Area type='monotone' dataKey='y' stroke='#fed057' strokeWidth={2} fillOpacity={1} fill='url(#color)'>
-            <LabelList fontSize={13} dataKey='y' position='top' fill='#ffffff99' offset={8} />
-          </Area>
-        </AreaChart>
-      </ResponsiveContainer>
-    </div>
+    <ResponsiveContainer className={cx(styles.chart, { [styles.active]: active })} width={500} height={250}>
+      <AreaChart margin={{ top: 20, right: 5, left: 5, bottom: 0 }} data={newData}>
+        <defs>
+          <linearGradient id='color' x1='0' y1='0' x2='0' y2='1'>
+            <stop offset='11%' stopColor='#fed057' stopOpacity={0.3} />
+            <stop
+              offset='85%'
+              stopColor={`${document.documentElement.getAttribute('theme') === 'day' ? '#3c6ad4' : '#101039'}`}
+              stopOpacity={0.1}
+            />
+          </linearGradient>
+        </defs>
+        <XAxis
+          padding={{ left: 10, right: 10 }}
+          stroke='#ffffff99'
+          tickLine={false}
+          tick={{ fill: '#ffffff99' }}
+          dataKey='x'
+        />
+        <Area type='monotone' dataKey='y' stroke='#fed057' strokeWidth={2} fillOpacity={1} fill='url(#color)'>
+          <LabelList fontSize={13} dataKey='y' position='top' fill='#ffffff99' offset={8} />
+        </Area>
+      </AreaChart>
+    </ResponsiveContainer>
   )
 }
 
