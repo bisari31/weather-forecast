dccbd051679ba7fb29caac22418025a1ec3c71ec
bisari <bisari31@gmail.com>
2022-06-17 17:37:30 +0900

    Style: 차트 라이브러리 변경

--- src/routes/chartList/ChartItem.tsx
+++ src/routes/chartList/ChartItem.tsx
@@ -1,20 +1,7 @@
-import { VictoryAxis, VictoryChart, VictoryGroup, VictoryLabel, VictoryLine } from 'victory'
-import {
-  LineChart,
-  Line,
-  Area,
-  XAxis,
-  AreaChart,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ComposedChart,
-  ResponsiveContainer,
-} from 'recharts'
+import { useEffect, useState } from 'react'
+import { Area, XAxis, AreaChart, ResponsiveContainer, LabelList } from 'recharts'
 import cx from 'classnames'
 import dayjs from 'dayjs'
-import { useEffect, useState } from 'react'
 
 import styles from './chartitem.module.scss'
 import { IDaliy } from 'types/weather'
@@ -30,18 +17,13 @@ interface IState {
   y: number
 }
 
-const Chart = ({ mean, active, data }: IProps) => {
+const ChartItem = ({ mean, active, data }: IProps) => {
   const [newData, setNewData] = useState<IState[]>()
-
-  const changePropMean = () => {
-    if (mean === 'Precipitation') return 'Precipitation (%)'
-    return 'Temperature (°C)'
-  }
 
   useEffect(() => {
     const changeData = () => {
       const filterData = data.map((item) => {
-        const target = Math.round(mean === 'Precipitation' ? item.pop * 100 : item.temp.max)
+        const target = Math.round(mean === 'rain' ? item.pop * 100 : item.temp.max)
         return { x: String(dayjs(item.dt * 1000).format('M.D')), y: target }
       })
       setNewData(filterData)
@@ -53,88 +35,32 @@ const Chart = ({ mean, active, data }: IProps) => {
 
   return (
     <div className={cx(styles.chart, { [styles.active]: active })}>
-      <ResponsiveContainer width='100%' height={300}>
-        <AreaChart className={cx(styles.chart, { [styles.active]: active })} data={newData}>
+      <ResponsiveContainer>
+        <AreaChart margin={{ top: 20, right: 5, left: 5, bottom: 0 }} data={newData}>
           <defs>
             <linearGradient id='color' x1='0' y1='0' x2='0' y2='1'>
               <stop offset='11%' stopColor='#fed057' stopOpacity={0.3} />
-              <stop offset='65%' stopColor='#101039' stopOpacity={0.1} />
+              <stop
+                offset='85%'
+                stopColor={`${document.documentElement.getAttribute('theme') === 'day' ? '#3c6ad4' : '#101039'}`}
+                stopOpacity={0.1}
+              />
             </linearGradient>
           </defs>
-          <XAxis dataKey='x' />
-          <Tooltip itemStyle={{ color: 'black' }} />
-          {/* <CartesianGrid vertical={false} stroke='#DDD' /> */}
-          <Area
-            isAnimationActive
-            type='monotone'
-            dataKey='y'
-            stroke='#fed057'
-            strokeWidth={2}
-            fillOpacity={1}
-            fill='url(#color)'
+          <XAxis
+            padding={{ left: 10, right: 10 }}
+            stroke='#ffffff99'
+            tickLine={false}
+            tick={{ fill: '#ffffff99' }}
+            dataKey='x'
           />
+          <Area type='monotone' dataKey='y' stroke='#fed057' strokeWidth={2} fillOpacity={1} fill='url(#color)'>
+            <LabelList fontSize={13} dataKey='y' position='top' fill='#ffffff99' offset={8} />
+          </Area>
         </AreaChart>
       </ResponsiveContainer>
     </div>
-    // <div className={cx(styles.chart, { [styles.active]: active })}>
-    //   <svg className={styles.gradient}>
-    //     <defs>
-    //       <linearGradient id='myGradient'>
-    //         <stop offset='24%' stopColor='#fed057' opacity='1' />
-    //         <stop offset='100%' stopColor='#fed057' opacity='0' />
-    //       </linearGradient>
-    //     </defs>
-    //   </svg>
-    //   <VictoryChart domainPadding={{ x: 15, y: 15 }} padding={{ top: 40, bottom: 40, left: 15, right: 15 }}>
-    //     <VictoryAxis
-    //       style={{
-    //         axis: { display: 'none' },
-    //         tickLabels: {
-    //           fontFamily: 'inherit',
-    //           fill: '#ffffff99',
-    //         },
-    //       }}
-    //     />
-    //     <VictoryGroup
-    //       data={newData}
-    //       animate={{
-    //         onLoad: {
-    //           duration: 500,
-    //         },
-    //         duration: 1000,
-    //       }}
-    //     >
-    //       <VictoryLabel
-    //         className={styles.label}
-    //         x={0}
-    //         y={10}
-    //         text={changePropMean}
-    //         style={{
-    //           fill: '#ffffff99',
-    //         }}
-    //       />
-    //       <VictoryLine
-    //         padding={{ top: 20, bottom: 60 }}
-    //         labels={({ datum }) => String(Math.round(datum.y))}
-    //         labelComponent={<VictoryLabel renderInPortal dy={-20} />}
-    //         style={{
-    //           labels: {
-    //             fontSize: '13',
-    //             fontFamily: 'inherit',
-    //             fill: '#ffffff99',
-    //           },
-    //           data: {
-    //             stroke: 'url(#myGradient)',
-    //             strokeWidth: 3,
-    //             strokeLinecap: 'round',
-    //           },
-    //         }}
-    //         interpolation='natural'
-    //       />
-    //     </VictoryGroup>
-    //   </VictoryChart>
-    // </div>
   )
 }
 
-export default Chart
+export default ChartItem
