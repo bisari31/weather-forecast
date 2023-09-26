282bd45c70360256482a236851352fd7e1e7176d
bisari <bisari31@gmail.com>
2023-09-26 21:19:35 +0900

    Refactor: react slick 적용

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ import { getWeatherForecast5DaysApi } from 'services/weather';
 import { geolocationState } from 'states/weather';
 
 import LocationWrapper from './location/LocationWrapper';
-// import DayList from './dayList/DayList';
+import WeeklyList from './weekly/WeeklyList';
 // import ChartList from './chartList/ChartList';
 
 const App = () => {
@@ -23,8 +23,8 @@ const App = () => {
   return (
     <div className={styles.wrapper}>
       <LocationWrapper results={results} />
-      {/* <DayList result={results[0].data} />
-      <ChartList result={results[0].data} /> */}
+      <WeeklyList result={results[0].data} />
+      {/* <ChartList result={results[0].data} />  */}
     </div>
   );
 };
